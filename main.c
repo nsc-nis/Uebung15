10ed8ddf14b49cf1de2f39b1616b72d09e6d16ab
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-03-18 10:36:00 +0100

    Program now fully functional

--- main.c
+++ main.c
@@ -7,6 +7,33 @@
  */
 int main()
 {
-    printf("Hello, World!\n");
+    printf("**********************************\n");
+    printf("* Welcome to NIS Number Guesser! *\n");
+    printf("*                                *\n");
+    printf("**********************************\n");
+
+    long int random(void);
+
+    int secret = random()%100 + 1;
+    int guess;
+    int true = -1;
+
+    while (true != 0)
+    {
+        printf("* --> Guess a number: \n");
+        scanf("%d", &guess);
+        if (guess == secret) {
+            printf("Correct! The number was %d\n", secret);
+            true = 0;
+        } else if (guess < secret) {
+            printf("Wrong! The number you are looking for is higher!\n");
+            printf("%d -> %d \n", guess, secret);
+            true = 1;
+        } else if (guess > secret) {
+            printf("Wrong! The number you are looking for is lower!\n");
+            printf("%d -> %d \n", guess, secret);
+            true = 1;
+        }
+    }
     return 0;
 }
